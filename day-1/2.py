eb1ed615236bb7522d53688a79bcdcfeb51b39de
imvikaspathak <imvikas.ai@gmail.com>
2022-07-05 19:24:44 +0530

    second commit

---
+++ day-1/2.py
@@ -0,0 +1,4 @@
+print("day 1 - string manipulation")
+print("string concatenation is done wih "+" sign.")
+print('e.g print("hello" + "world")')
+print("new lone can be created with a backslash and n.")
